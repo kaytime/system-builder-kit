8cd6b15846bb899cb829d6a00a72e1f28c437768
Steph DGenius <stephanetse@gmail.com>
2022-09-14 17:16:14 +0100

    🐛 Fix builder function import

--- configs/scripts/builder/main.sh
+++ configs/scripts/builder/main.sh
@@ -1,5 +1,5 @@
 for entry in /configs/scripts/builder/*.sh; do
-    if [ "$entry" != "main.sh" ]; then
-        source /configs/scripts/builder/$entry
+    if [ "$entry" != "/configs/scripts/builder/main.sh" ]; then
+        source $entry
     fi
 done
