5c531e0c1423c3fc78772269b6cb9d1e06b9e56b
Steph DGenius <stephanetse@gmail.com>
2022-09-15 02:03:19 +0100

    🔥 Disable compat repo

--- configs/scripts/builder/adding_sources_file.sh
+++ configs/scripts/builder/adding_sources_file.sh
@@ -23,13 +23,13 @@ while :; do
 done
 
 adding_sources_file() {
-    cp configs/files/sources.list.kaytime /etc/apt/sources.list
+    # cp configs/files/sources.list.kaytime /etc/apt/sources.list
     cp configs/files/sources.list.kaytime.$BUILD_CHANNEL /etc/apt/sources.list.d/kaytime-$BUILD_CHANNEL-repo.list
     cp configs/files/sources.list.debian.experimental /etc/apt/sources.list.d/debian-experimental-repo.list
     cp configs/files/sources.list.debian.unstable /etc/apt/sources.list.d/debian-unstable-repo.list
 
     apt-key export $BUILD_CHANNEL_APT_KEY | gpg --dearmour -o /usr/share/keyrings/kaytime-$BUILD_CHANNEL.gpg
-    apt-key export 176991DF5EA0105E | gpg --dearmour -o /usr/share/keyrings/kaytime-compat.gpg
+    # apt-key export 176991DF5EA0105E | gpg --dearmour -o /usr/share/keyrings/kaytime-compat.gpg
 
     update
 }
